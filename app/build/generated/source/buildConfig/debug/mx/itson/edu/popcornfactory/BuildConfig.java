299bdaa0726b598e99f52d45ee84dc73bcf2c35a
Luis Marin <la.marin@outlook.com>
2023-03-01 12:39:37 -0700

    First Commit

---
+++ app/build/generated/source/buildConfig/debug/mx/itson/edu/popcornfactory/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package mx.itson.edu.popcornfactory;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "mx.itson.edu.popcornfactory";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
