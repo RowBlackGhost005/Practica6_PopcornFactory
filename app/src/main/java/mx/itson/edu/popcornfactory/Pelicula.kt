fd3dc1f93d0668deb0b00410b94f601e22e69ad0
Luis Marin <la.marin@outlook.com>
2023-03-01 12:20:36 -0700

    Revert "Practica 6"
    This reverts commit 54c88e73cbb930ad399beb79f5260a5f8b17532f.

--- app/src/main/java/mx/itson/edu/popcornfactory/Pelicula.kt
+++
@@ -1,6 +0,0 @@
-package mx.itson.edu.popcornfactory
-
-data class Pelicula (var titulo:String,
-                     var image:Int,
-                     var header:Int,
-                     var sinopsis:String)
